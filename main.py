8ace6eb0262329321b8be9b762d0c62b4ab65e8b
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-10-14 17:09:24 +0000

    random numbers + even/odd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, shuffle
   # random
   # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only 
   #show you two useful functions for now.
@@ -11,3 +11,23 @@ rolledDoubles = dice1 == dice2
 if rolledDoubles:
   print("you rolled doubles")
 else: print("not doubles")
+
+# create a way to roll snakes
+if dice1 and dice2 == 1:
+  print("you rolled snake eyes")
+else: print("you didn't roll snake eyes")
+
+# practice with shuffle
+my_list = range(1,51)
+print("my new list")
+print(list(my_list))
+my_list = list(my_list)
+shuffle(my_list)
+print(my_list)
+
+mylist = randint(1,201)
+print("new list")
+print(mylist)
+if mylist %2 == 0:
+  print("number is even")
+else: print("number is odd")
